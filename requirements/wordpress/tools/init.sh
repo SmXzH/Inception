c985d81de0a5632cf181370c6b720fcd2e113659
Sm4zH <97186566+Sm4zH@users.noreply.github.com>
2023-07-17 23:55:44 +0400

    fixed bugs added script for creating wordpress connected for databse

--- requirements/wordpress/tools/init.sh
+++ requirements/wordpress/tools/init.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+cd /var/www/html/wordpress
+
+
+sed -i "s/define( 'DB_NAME', 'wordpress' );/define( 'DB_NAME', '$DB_NAME' );/g" wp-config.php
+sed -i "s/define( 'DB_PASSWORD', 'hello123' );/define( 'DB_PASSWORD', '$DB_PASS' );/g" wp-config.php
+sed -i "s/define( 'DB_USER', 'kabusitt' );/define( 'DB_USER', '$DB_USER' );/g" wp-config.php
+
 wp core download --force --allow-root
 
 if ! wp core is-installed --allow-root --path=/var/www/html/wordpress; then
-wp core install --url=localhost --title=test --admin_user=szhakypo --admin_password=test123 --admin_email=szhakypo@student.42abudhabi.ae --allow-root
-wp user create szhakypouser szhakypo@student.42abudhabi.ae --role=author --user_pass=test123 --allow-root
+wp core install --url=$WP_URL --title=test --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --allow-root
+wp user create $WP_USER $WP_EMAIL --role=subscriber --user_pass=$WP_PASS --allow-root
 fi
 
 /usr/sbin/php-fpm7.3 -F -R
